22186ada9b7f37ec25ecaaecb147d7a45045fbb2
Harvey Shimizu <sshidecap.portable@gmail.com>
2023-04-19 21:33:31 +0900

    update analize.sh

--- analize.sh
+++ analize.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-dirList=(`ls -l $@ | awk '/^d/ {print $9}'`)
 total=(`gfind $@ -name "*.[ch]" -type f | xargs wc --total=only -l | awk '{sum+=$1} END {print sum}'`)
-
 echo "total: " $total
 
+files=(`gfind $@ -maxdepth 1 -name "*.[ch]" -type f | xargs wc --total=only -l | awk '{sum+=$1} END {print sum}'`)
+percent=(`echo "scale=7; ${files}/${total}*100" | bc`)
+echo "." $files "\t" $percent "%"
+
+dirList=(`ls -l $@ | awk '$0~/^d/ && $9~/\<[a-z]/ {print $9}'`)
 for dir in ${dirList[@]}
 do
-    line=(`gfind ./$@/${dir} -name "*.[ch]" -type f | xargs wc --total=only -l | awk '{sum+=$1} END {print sum}'`)
-    percent=(`echo "scale=6; ${line}/${total}*100" | bc`)
+    line=(`gfind $@/${dir} -name "*.[ch]" -type f | xargs wc --total=only -l | awk '{sum+=$1} END {print sum}'`)
+    percent=(`echo "scale=7; ${line}/${total}*100" | bc`)
     echo $dir "\t" $line "\t" $percent "%"
 done
