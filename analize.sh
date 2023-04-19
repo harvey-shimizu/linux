8b3b749ad32b1d2eebf0a596cfaf844a6467abf3
Harvey Shimizu <sshidecap.portable@gmail.com>
2023-04-19 19:47:26 +0900

    update the script

--- analize.sh
+++ analize.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-dirList=(`ls -lga | grep ^d | awk '{print $8}' | tail -n +6`)
+dirList=(`ls -l $@ | awk '/^d/ {print $9}'`)
+total=(`gfind $@ -name "*.[ch]" -type f | xargs wc --total=only -l | awk '{sum+=$1} END {print sum}'`)
 
-total=(`gfind . -regex \.\/.*\.[ch] -type f | xargs wc --total=only -l | awk '{x+=$1} END {print x}'`)
 echo "total: " $total
 
 for dir in ${dirList[@]}
 do
-    line=(`gfind . -regex \.\/\${dir}.*\.[ch] -type f | xargs wc --total=only -l | awk '{x+=$1} END {print x}'`)
+    line=(`gfind ./$@/${dir} -name "*.[ch]" -type f | xargs wc --total=only -l | awk '{sum+=$1} END {print sum}'`)
     percent=(`echo "scale=6; ${line}/${total}*100" | bc`)
     echo $dir "\t" $line "\t" $percent "%"
 done
