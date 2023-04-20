fbd41917f16208172aba22aed5e80c943d5c7559
Harvey Shimizu <sshidecap.portable@gmail.com>
2023-04-20 09:32:57 +0900

    change gfind to find

--- analize.sh
+++ analize.sh
@@ -1,16 +1,16 @@
-#!/bin/sh
+#!/bin/zsh
 
-total=(`gfind $@ -name "*.[ch]" -type f | xargs wc --total=only -l | awk '{sum+=$1} END {print sum}'`)
+total=(`find $@ -name "*.[ch]" -type f | xargs wc --total=only -l | awk '{sum+=$1} END {print sum}'`)
 echo "total: " $total
 
-files=(`gfind $@ -maxdepth 1 -name "*.[ch]" -type f | xargs wc --total=only -l | awk '{sum+=$1} END {print sum}'`)
+files=(`find $@ -maxdepth 1 -name "*.[ch]" -type f | xargs wc --total=only -l | awk '{sum+=$1} END {print sum}'`)
 percent=(`echo "scale=7; ${files}/${total}*100" | bc`)
 echo "." $files "\t" $percent "%"
 
 dirList=(`ls -l $@ | awk '$0~/^d/ && $9~/\<[a-z]/ {print $9}'`)
 for dir in ${dirList[@]}
 do
-    line=(`gfind $@/${dir} -name "*.[ch]" -type f | xargs wc --total=only -l | awk '{sum+=$1} END {print sum}'`)
+    line=(`find $@/${dir} -name "*.[ch]" -type f | xargs wc --total=only -l | awk '{sum+=$1} END {print sum}'`)
     percent=(`echo "scale=7; ${line}/${total}*100" | bc`)
     echo $dir "\t" $line "\t" $percent "%"
 done
