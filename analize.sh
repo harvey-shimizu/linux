7b2087aa036e010428b4de0871ade4f6b48f7d04
Harvey Shimizu <sshidecap.portable@gmail.com>
2023-04-20 20:49:41 +0900

    remove %

--- analize.sh
+++ analize.sh
@@ -5,12 +5,12 @@ echo "total: " $total
 
 files=(`find $@ -maxdepth 1 -name "*.[ch]" -type f | xargs wc --total=only -l | awk '{sum+=$1} END {print sum}'`)
 percent=(`echo "scale=7; ${files}/${total}*100" | bc`)
-echo "." $files "\t" $percent "%"
+echo "." $files "\t" $percent
 
 dirList=(`ls -l $@ | awk '$0~/^d/ && $9~/\<[a-z]/ {print $9}'`)
 for dir in ${dirList[@]}
 do
     line=(`find $@/${dir} -name "*.[ch]" -type f | xargs wc --total=only -l | awk '{sum+=$1} END {print sum}'`)
     percent=(`echo "scale=7; ${line}/${total}*100" | bc`)
-    echo $dir "\t" $line "\t" $percent "%"
+    echo $dir "\t" $line "\t" $percent
 done
